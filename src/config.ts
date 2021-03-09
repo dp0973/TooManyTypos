3420b0673db62c4f584eb4529eb6e20cef043bd1
ombe <h3236516@gmail.com>
2021-03-09 16:39:44 +0900

    헤로쿠 수정

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
-    token: process.env["token"], // 디스코드 봇 토큰
-    author: process.env["author"], // 오타 수정해줄 유저 id
-    naverClientId: process.env["naverClientId"], // 네이버 API 클라 id
-    naverClientSecret: process.env["naverClientSecret"], // 네이버 API 클라 시크릿
+    token: process.env.token, // 디스코드 봇 토큰
+    author: process.env.author, // 오타 수정해줄 유저 id
+    naverClientId: process.env.naverClientId, // 네이버 API 클라 id
+    naverClientSecret: process.env.naverClientSecret, // 네이버 API 클라 시크릿
 }
